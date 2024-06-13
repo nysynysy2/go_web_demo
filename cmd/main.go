b4d042c738ed26a909cb375757643239fd671c9b
nysynysy2 <nysynysy2@qq.com>
2024-06-13 21:30:29 +0800

    Mobile main.go file deprecated; Mysql dsn fix

--- cmd/main.go
+++ cmd/main.go
@@ -41,7 +41,7 @@ type Dept struct {
 var db *gorm.DB
 
 func init() {
-	res, err := gorm.Open(mysql.Open("root:NYSY2nysy%40mysql!tcp(127.0.0.1:3306)/day10?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	res, err := gorm.Open(mysql.Open("root:NYSY2nysy%40@mysql!tcp(127.0.0.1:3306)/day10?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Error connecting to database:%e", err)
 	}
